d9821e119e9748b299253074729dd87aae3df972
William Ntomani <wntomani@yahoo.com>
2018-11-24 21:11:48 -0500

    Commit5: Restore helloworld.java from commit 564250e

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ public class Helloworld {
     public static void main(String[] args) {
          // Prints "Hello, World" in the terminal windows.
          system.out.println("Hello, World");
-#This java code will print Welcome to Git
+
          system.out.println("Welcome to Git");
     }
 }
