564250e36fa1e2cec8d9fde08d4d4aeec6c61024
William Ntomani <wntomani@yahoo.com>
2018-11-24 20:48:40 -0500

    Commit3: Added new print statement

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,7 @@ public class Helloworld {
     public static void main(String[] args) {
          // Prints "Hello, World" in the terminal windows.
          system.out.println("Hello, World");
+
+         system.out.println("Welcome to Git");
     }
 }
